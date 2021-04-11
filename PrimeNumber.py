3741fe8abfd8832f103eab23e794387515073fd5
amolthakurdware <51024406+amolthakurdware@users.noreply.github.com>
2021-04-11 17:17:44 +0530

    Create PrimeNumber.py

---
+++ PrimeNumber.py
@@ -0,0 +1,12 @@
+def is_prime(number):
+    is_prime=True
+    for devisor in range(2,number):
+        remainder=number%devisor
+        if remainder==0:
+            is_prime=False
+            break            
+    return is_prime
+
+for number in range(2,11):
+    if is_prime(number):
+        print(number)
