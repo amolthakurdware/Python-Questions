bfd0d7b76eb75b1a3ba7601b2aca21aef2c3e417
amolthakurdware <51024406+amolthakurdware@users.noreply.github.com>
2021-04-12 11:32:25 +0530

    Create factorial_while_for.py

---
+++ factorial_while_for.py
@@ -0,0 +1,15 @@
+# Video link: https://youtu.be/cVT-vX-jv6k
+import math
+number=4
+factorial=1
+
+# for i in range(1,number+1):
+#     factorial=factorial*i
+
+i=1
+while i<=number  :
+    factorial=factorial*i
+    i=i+1
+
+print(factorial)
+#print(math.factorial(number))
