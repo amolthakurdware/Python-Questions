cafaa54ef1f4e73b4060abd654f16294d835b76f
amolthakurdware <51024406+amolthakurdware@users.noreply.github.com>
2021-04-12 08:32:53 +0530

    Create factorial_function.py

---
+++ factorial_function.py
@@ -0,0 +1,13 @@
+def find_factorial(number):
+    factorial=1
+    if(number<0):
+        print("Factorial of a negative number can not be calcualted")
+        return
+    for i in range(1,number+1):
+        factorial=factorial*i
+    return factorial
+
+
+number=int(input("Enter a number:"))
+factorial=find_factorial(number)
+print("Factorial of ",number," is ",factorial)
