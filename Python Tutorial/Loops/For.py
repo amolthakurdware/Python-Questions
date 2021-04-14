e6fdd54c3972fd7f03502006f696177c1c822639
amolthakurdware <51024406+amolthakurdware@users.noreply.github.com>
2021-04-14 16:52:10 +0530

    Deleted the files

--- Python Tutorial/Loops/For.py
+++
@@ -1,3 +0,0 @@
-# Video: https://youtu.be/hU0D_YcvmBQ
-for i in range(1,5):
-    print("Beer")
--- Python Tutorial/Loops/ForNested.py
+++
@@ -1,5 +0,0 @@
-# Video: https://youtu.be/hU0D_YcvmBQ
-for i in range(1,5):
-    print("Beer")
-    for j in range(1,3):
-        print("Vodka")
